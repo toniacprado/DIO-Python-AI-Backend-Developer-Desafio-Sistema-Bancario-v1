fc55029a91a843d6be9f49a76173eb04de2f5219
toniacprado <toniacprado@gmail.com>
2024-04-12 21:07:30 -0300

    Em desafio.py adicionado tratamento sinal negativo nos saques

--- desafio.py
+++ desafio.py
@@ -1,5 +1,7 @@
 # Resposta para o desafio v1. do Bootcamp Bootcamp Coding The Future Vivo - Python AI Backend Developer
 # Objetivo: Criar um sistema bancário com as operações: sacar, depositar e visualizar extrato.
+# As regras de negócio das operações estão descritas no README.md do repositório no gitHub
+# https://github.com/toniacprado/DIO-Python-AI-Backend-Developer-Desafio-Sistema-Bancario-v1
 # Autor: Toni A C Prado
 
 menu = """
@@ -17,7 +19,7 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 
-# Função tratar os inputs e aceitar somente entrada de valores numéricos
+# Função para tratar os inputs e aceitar somente entrada de valores numéricos
 def input_float(prompt):
     while True:
         try:
@@ -40,7 +42,8 @@ while True:
             print("Entrada inválida, favor digitar um valor numerico positivo a ser depositado")
     elif opcao.upper() == "S":
         if numero_saques < LIMITE_SAQUES:
-            valor = input_float(f"O limite por saque é R$ {limite:.2f}. Digite o valor que deseja sacar: ")
+            valor = abs(input_float(f"O limite por saque é R$ {limite:.2f}. Digite o valor que deseja sacar: "))
+            # a função abs vai tratar e validar para que independente do sinal digitado pelo usuário ele faça o débito no saldo corretamente.
             
             if valor > limite:
                 print(f"Valor inválido, o máximo permido por saque é de R$ {limite:.2f}. ")
@@ -56,7 +59,7 @@ while True:
     elif opcao.upper() == "E":
         print("\n================ EXTRATO =================")
         print(extrato)
-        print(f"Saldo Atual: R$ {saldo:.2f}.")
+        print(f"Saldo Atual: R$ {saldo:.2f}")
         print("==========================================")
     elif opcao.upper() == "Q":
         print("The Bootcamp Bank agradece a sua visita, volte sempre!")
